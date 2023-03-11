4e811144442c305d25e4c4df1e609205e774000b
trankimhoang <trankimhoang11052000@gmail.com>
2023-03-11 17:23:51 +0700

    update attr data

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 $(document).ready(function (){
     console.log($('.sova').offset().top);
 
-    $('.menutren ul li:nth-child(1) a').click(function (event){
+    $('.menutren ul li a').click(function (event){
         event.preventDefault();
-        $('html, body').animate({ scrollTop: $('.sova').offset().top},1400,"easeOutCubic");
-    });
 
-    $('.menutren ul li:nth-child(2) a').click(function (event){
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: $('.sage').offset().top},1400,"easeOutExpo");
-    });
+        let target = $(this).attr('data-target');
 
-    $('.menutren ul li:nth-child(3) a').click(function (event){
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: $('.reyna').offset().top},1400,"easeOutExpo");
-    });
-
-    $('.menutren ul li:nth-child(4) a').click(function (event){
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: $('.jett').offset().top},1400,"easeOutExpo");
-    });
-
-    $('.menutren ul li:nth-child(5) a').click(function (event){
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: $('.phoenix').offset().top},1400,"easeOutExpo");
+        $('html, body').animate({ scrollTop: $(target).offset().top},1400,"easeOutCubic");
     });
 
 // viet cho nut back to top
